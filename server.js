db448489d5771f162a64bc0a80f35948f6522f2e
Eric <eric@Erics-MacBook-Pro.local>
2020-03-14 08:42:40 -0400

    update: run the app via express for backend APIs.

---
+++ server.js
@@ -0,0 +1,79 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const redirectSSL = require('redirect-ssl');
+const nodeMailer = require("nodemailer");
+const Bundler = require("parcel-bundler");
+const Path = require("path");
+
+const file = Path.join(__dirname, "./html/index.html"); // Pass an absolute path to the entrypoint here
+const options = {
+  outDir: "./dist", // The out directory to put the build files in, defaults to dist
+  outFile: "index.html", // The name of the outputFile
+  publicUrl: "/", // The url to serve on, defaults to '/'
+  watch: true, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
+  cache: false, // Enabled or disables caching, defaults to true
+  minify: false, // Minify files, enabled if process.env.NODE_ENV === 'production'
+  scopeHoist: false, // Turn on experimental scope hoisting/tree shaking flag, for smaller production bundles
+  target: "browser", // Browser/node/electron, defaults to browser
+  bundleNodeModules: false, // By default, package.json dependencies are not included when using 'node' or 'electron' with 'target' option above. Set to true to adds them to the bundle, false by default
+  https: { // Define a custom {key, cert} pair, use true to generate one or false to use http
+    cert: './ssl/c.crt', // Path to custom certificate
+    key: './ssl/k.key' // Path to custom key
+  },
+  logLevel: 3, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors, 0 = log nothing
+  hmr: true, // Enable or disable HMR while watching
+  hmrPort: 0, // The port the HMR socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
+  sourceMaps: false, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
+  detailedReport: false, // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
+  autoInstall: true // Enable or disable auto install of missing dependencies found during bundling
+};
+
+// app.use(redirectSSL);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(redirectSSL);
+
+app.post("/api/mailer", function(req, res, next) {
+  let transporter = nodeMailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  if (transporter) {
+    let mailOptions = {
+      from: process.env.EMAIL_ADDRESS,
+      to: process.env.EMAIL_ADDRESS,
+      subject: "New signup from campbikewinetour.com!",
+      text: `Someone signed up for CBWT updates.
+
+      Name: ${req.body.name}
+      Email: (${req.body.email})`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        res.send({
+          formDidFail: true
+        });
+      } else {
+        res.send({
+          formDidFail: false
+        });
+      }
+    });
+  }
+});
+
+// Initialize a new bundler using a file and options
+const bundler = new Bundler(file, options);
+// Let express use the bundler middleware, this will let Parcel handle every request over your express server
+app.use(bundler.middleware());
+
+// Export the server middleware
+app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`));
--- src/main.js
+++ src/main.js
@@ -8,9 +8,40 @@ void (() => {
   let $daysEl = undefined;
   let $dateEl = undefined;
   let $yearDate = undefined;
+  let $formEl = undefined;
   let $formName = undefined;
   let $formEmail = undefined;
   let $formSubmit = undefined;
+  let $formDummyPost = undefined;
+  let $formOverlayEl = undefined;
+  let $formOverlayElText = undefined;
+
+  const _bind = () => {
+    $memoriumLink = document.querySelector(".memorium-link");
+    $memoriumCloseBtn = document.querySelector(".memorium-close-btn");
+    $memoriumEl = document.querySelector("#memoriam");
+    $mobileNavBtn = document.querySelector(".mobile-nav-btn");
+    $mobileNavCloseBtn = document.querySelector(".nav-bar--close");
+    $mobileNavEl = document.querySelector(".nav-bar--nav");
+    $daysEl = document.querySelector(".day-counter .days");
+    $dateEl = document.querySelector(".trip-date .date");
+    $yearDate = Array.from(document.querySelectorAll(".year-date"));
+    $formEl = document.querySelector("#signup-form");
+    $formName = document.querySelector("#signup-form--name");
+    $formEmail = document.querySelector("#signup-form--email");
+    $formSubmit = document.querySelector("#signup-form--submit");
+    $formDummyPost = document.querySelector("#signup-form--dummy");
+    $formOverlayEl = document.querySelector("#signup-form--overlay");
+    $formOverlayElText = document.querySelector("#signup-form--overlay .overlay-text");
+  };
+
+  const _listen = () => {
+    $memoriumLink.addEventListener("click", _toggleMemoriumView);
+    $memoriumCloseBtn.addEventListener("click", _toggleMemoriumView);
+    $mobileNavBtn.addEventListener("click", _toggleMobileNavView);
+    $mobileNavCloseBtn.addEventListener("click", _toggleMobileNavView);
+    $formSubmit.addEventListener("click", _submitContactForm);
+  };
 
   const _toggleMemoriumView = () => {
     $memoriumEl.classList.toggle("hidden");
@@ -73,59 +104,53 @@ void (() => {
     $dateEl.innerHTML = `Aug ${prettyFirstThursdayDate} - ${parseInt(prettyFirstThursdayDate) + 4}`;
   };
 
-  const _sendEmail = () => {
-    const formName = $formName.value;
-    const formEmail = $formEmail.value;
-    const canSubmit = formName.length > 0 && formEmail.length > 0;
-    const emailMsg = {
-      Host: "smtp.gmail.com",
-      Username: process.env.EMAIL_ADDRESS,
-      Password: process.env.EMAIL_PASSWORD,
-      To: process.env.EMAIL_ADDRESS,
-      From: process.env.EMAIL_ADDRESS,
-      Subject: "A new sign-up from campbikewinetour.com!",
-      Body: `Someone has signed up to get notified about CBWT updates. Here is their info:
+  const _submitContactForm = () => {
+    // Try to avoid bots
+    if ($formDummyPost.value.length > 0) {
+      return;
+    }
+    // Form is invalid
+    if (!$formEl.checkValidity()) {
+      //
+    } else {
+      fetch("/api/mailer", {
+        method: "POST",
+        mode: "cors",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name: $formName.value,
+          email: $formEmail.value
+        })
+      })
+        .then(res => res.json())
+        .then(res => {
+          const { formDidFail } = res;
 
-      Name: ${formName}
-      Email: ${formEmail}`
-    };
+          if( formDidFail ){
+            $formOverlayElText.innerHTML = "Sorry, there was a problem. Please try again later."
+            $formOverlayElText.classList.remove('success');
+            $formOverlayElText.classList.add('error');
+          } else {
+            $formOverlayElText.innerHTML = "Thanks for signing up!"
+            $formOverlayElText.classList.remove('error');
+            $formOverlayElText.classList.add('success');
+            $formEl.reset();
+          }
 
-    console.log(process.env.EMAIL_ADDRESS);
+          $formOverlayEl.classList.remove("hidden");
+          $formOverlayEl.classList.add('fade');
 
-    if (canSubmit && window.Email) {
-      window.Email.send(emailMsg)
-        .then(msg => {
-          alert("success");
+          window.setTimeout( () => {
+            $formOverlayEl.classList.add("hidden");
+          }, 5999);
         })
         .catch(err => {
-          alert("faiure");
+          console.error(err);
         });
-    } else {
-      // alert("mail did not send");
     }
-  };
-
-  const _bind = () => {
-    $memoriumLink = document.querySelector(".memorium-link");
-    $memoriumCloseBtn = document.querySelector(".memorium-close-btn");
-    $memoriumEl = document.querySelector("#memoriam");
-    $mobileNavBtn = document.querySelector(".mobile-nav-btn");
-    $mobileNavCloseBtn = document.querySelector(".nav-bar--close");
-    $mobileNavEl = document.querySelector(".nav-bar--nav");
-    $daysEl = document.querySelector(".day-counter .days");
-    $dateEl = document.querySelector(".trip-date .date");
-    $yearDate = Array.from(document.querySelectorAll(".year-date"));
-    $formName = document.querySelector("#signup-form--name");
-    $formEmail = document.querySelector("#signup-form--email");
-    $formSubmit = document.querySelector("#signup-form--submit");
-  };
-
-  const _listen = () => {
-    $memoriumLink.addEventListener("click", _toggleMemoriumView);
-    $memoriumCloseBtn.addEventListener("click", _toggleMemoriumView);
-    $mobileNavBtn.addEventListener("click", _toggleMobileNavView);
-    $mobileNavCloseBtn.addEventListener("click", _toggleMobileNavView);
-    $formSubmit.addEventListener("click", _sendEmail);
   };
 
   const _init = () => {
@@ -133,63 +158,7 @@ void (() => {
     _listen();
     _setYearDate();
     _getFirstThursday();
-    // _bindFormHandler();
   };
 
   document.addEventListener("DOMContentLoaded", _init);
 })(document);
-
-// function bindFormHandler(){
-//   $( "#signup-form" ).submit( function( event ){
-//     event.preventDefault(); // avoid to execute the actual submit of the form.
-//     var form = $( this );
-//     // recpatcha is not filled out
-//     if( grecaptcha.getResponse().length === 0 ){
-//       // show the reminder
-//       $('.recaptcha-title').removeClass('hidden');
-//     } else {
-//       request = $.ajax({
-//         type: "POST",
-//         url: "includes/php/mailer.php",
-//         data: form.serialize(), // serializes the form's elements.
-//         success: function( data, textStatus, jqXHR ){
-//           form.trigger('reset');
-//           grecaptcha.reset();
-//           // console.log( data )
-//           returnedData = JSON.parse( data );
-//           console.log( returnedData.notes )
-//           try {
-//             if( returnedData.fail === true ){
-
-//               $('#fail-msg p').text( returnedData.notes );
-//               $('#fail-msg').removeClass('hidden');
-
-//               setTimeout(function(){
-//                 $('#fail-msg').addClass('hidden');
-//               }, 5000);
-//             }
-//           } catch( err ){
-//             console.log( "Form invalid" );
-//           }
-
-//           try {
-//             if( returnedData.success === true ){
-//               $('#success-msg').removeClass('hidden');
-//               setTimeout(function(){
-//                 $('#success-msg').addClass('hidden');
-//               }, 5000);
-//             }
-//           } catch( err ) {
-//             console.log( "JSON Parse error from server." )
-//           }
-//         },
-//       });
-
-//       request.fail( function( jqXHR, textStatus, errorThrown ){
-//         console.log( "Request has failed." )
-//         form.trigger('reset');
-//         grecaptcha.reset();
-//       });
-//     };
-//   });
-// };
